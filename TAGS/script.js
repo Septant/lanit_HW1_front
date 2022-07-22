894904fb20a04eaa8a5319e2bfc111da6461edbb
Septant <orcams@yandex.ru>
2022-07-22 21:07:54 +0300

    URL second commit script.js: all .innerHTML -> .textContent

---
+++ TAGS/script.js
@@ -0,0 +1,7 @@
+let source1 = URL("https://www.w3schools.com/html/html_forms_attributes.asp");
+let source2 = URL("https://learn.javascript.ru/object#tsikl-for-in");
+let source3 = URL("https://angdev.ru/doc/angular-and-rxjs/");
+let source4 = URL("https://material.angular.io/components/table/examples");
+let source5 = URL("https://nodejsdev.ru/doc/rest-api/#rest-api_2");
+
+document.getElementById("src1").innerHTML = source1;
--- URL/script.js
+++ URL/script.js
@@ -1,6 +1,6 @@
 let url = new URL("http://localhost:63342/URL/index.html?_ijt=jvq04d7e94ne24uim9gt92jpvb&_ij_reload=RELOAD_ON_SAVE");
 
-document.getElementById("link").innerHTML = url.toString();
+document.getElementById("link").textContent = url.toString();
 
 let protocol = url.protocol;
 let leftPath = url.toString().split('.')[0];
@@ -9,9 +9,9 @@ let rightPath = url.toString().split('.')[1];
 let fileName = leftPath.replace(/^.*[\\\/]/,'');
 let fileExtension = rightPath.replace(/\?.*/,'');
 
-document.getElementById("fileName").innerHTML = fileName;
-document.getElementById("fileExtension").innerHTML = fileExtension;
-document.getElementById("protocol").innerHTML = protocol;
+document.getElementById("fileName").textContent = fileName;
+document.getElementById("fileExtension").textContent = fileExtension;
+document.getElementById("protocol").textContent = protocol;
 
 params = {};
 let paramsToPrint = "";
@@ -21,4 +21,4 @@ for (let [name, value] of url.searchParams) {
     paramsToPrint += "[" + name +"="+ value + "] ";
 }
 
-document.getElementById("params").innerHTML = paramsToPrint;
+document.getElementById("params").textContent = paramsToPrint;
