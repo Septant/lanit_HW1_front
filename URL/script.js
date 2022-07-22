6ee851f5e2d4d474a6cf1bf382ef22394c3749dc
Septant <orcams@yandex.ru>
2022-07-22 21:08:36 +0300

    URL second commit script.js: all .innerHTML -> .textContent

--- URL/script.js
+++ URL/script.js
@@ -3,6 +3,7 @@ let url = new URL("http://localhost:63342/URL/index.html?_ijt=jvq04d7e94ne24uim9
 document.getElementById("link").textContent = url.toString();
 
 let protocol = url.protocol;
+
 let leftPath = url.toString().split('.')[0];
 let rightPath = url.toString().split('.')[1];
 
