963aa5045f53b052bbe9889f493186ca22786f24
Septant <orcams@yandex.ru>
2022-07-25 11:35:41 +0300

    TAGS first commit
    Task finished

--- TAGS/script.js
+++ TAGS/script.js
@@ -1,7 +1,39 @@
-let source1 = URL("https://www.w3schools.com/html/html_forms_attributes.asp");
-let source2 = URL("https://learn.javascript.ru/object#tsikl-for-in");
-let source3 = URL("https://angdev.ru/doc/angular-and-rxjs/");
-let source4 = URL("https://material.angular.io/components/table/examples");
-let source5 = URL("https://nodejsdev.ru/doc/rest-api/#rest-api_2");
+source1 = new URL("https://www.w3schools.com/html/html_forms_attributes.asp");
+source2 = new URL("https://learn.javascript.ru/object#tsikl-for-in");
+source3 = new URL("https://angdev.ru/doc/angular-and-rxjs/");
+source4 = new URL("https://material.angular.io/components/table/examples");
+source5 = new URL("https://nodejsdev.ru/doc/rest-api/#rest-api_2");
 
-document.getElementById("src1").innerHTML = source1;
+document.getElementById("src1").textContent = source1.toString();
+document.getElementById("src2").textContent = source2.toString();
+document.getElementById("src3").textContent = source3.toString();
+document.getElementById("src4").textContent = source4.toString();
+document.getElementById("src5").textContent = source5.toString();
+
+srcList = document.querySelectorAll("a");
+document.getElementById("srcAmount").textContent = srcList.length.toString();
+
+document.getElementById("innerHTML").innerHTML = source1;
+
+picsList = document.querySelectorAll("img");
+console.log(picsList);
+document.getElementById("picsAmount").textContent = picsList.length.toString();
+
+firstPic = document.getElementById("helicopter");
+fpWidth = window.getComputedStyle(firstPic).width;
+
+
+let picsWidth = 0;
+let maxSize = 0.00;
+
+for (let pic of picsList) {
+    if(pic.width > maxSize){
+        maxSize = pic.width;
+        console.log(maxSize);
+    }
+    picsWidth += pic.height;
+}
+
+document.getElementById("dpWidth").textContent = `${fpWidth.toString()} и ${maxSize.toString()}px соответственно.`;
+
+document.getElementById("picsWidth").textContent = `${picsWidth.toString()}px`;
